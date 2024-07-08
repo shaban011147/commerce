8b29b38e147e7f74db1046b9081f9f4f129f252a
moonsuntow <134846251+moonsuntow@users.noreply.github.com>
2024-07-09 01:26:59 +0300

    initial commit

--- src/assets/js/test.js
+++ src/assets/js/test.js
@@ -1,3 +1,10 @@
 export const test = () => {
     console.log('from test');
-}
+};
+
+let none = document.getElementById("none");
+let remove = document.getElementById("remove");
+
+remove.onclick = function () {
+    none.style.display = "none";
+};
